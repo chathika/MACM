6ba825bb82715d18bc2a877f228e639ee7a16020
deamonpog <pog666@gmail.com>
2020-01-17 10:06:16 +0000

    updated post processing script

--- MACMGPU_EventLog_To_JSON.py
+++ MACMGPU_EventLog_To_JSON.py
@@ -14,7 +14,7 @@ _Sim_StartTime = '2017.04.01'
 _Sim_EndTime = '2018.03.31'
 _CurrentSprint = 14
 _CommitSha = '05be2aaf9e8d61ecc2b48cf0735028db2a3f0bce'
-_output_directory_path = "/home/social-sim/MACMWorking/MACM/Output_manual"
+_output_directory_path = "/home/social-sim/MACMWorking/MACM/Ready_manual/"
 
 _nodelist_file = '/home/social-sim/MACMWorking/MACM/DryRun/Inputs/cp3_dry_run_s1_nodelist_updated.txt'
 
@@ -26,12 +26,16 @@ _RunNumber = 0
 _mr_csv_folder = '/home/social-sim/MACMWorking/MACM/FactorialRun/Original'
 
 # Single File Options
-_file_csv_eventlog = '/home/social-sim/MACMWorking/MACM/Output_manual/MACM_MMD10_Alpha0.8_2020-01-14 20_12_49.260452.csv'
+_file_csv_eventlog = '/home/social-sim/MACMWorking/MACM/Output_manual/MACM_MMD30_Alpha0.8_2020-01-17 09_14_46.988908.csv'
 _file_csv_S3Location = 'null'
-_Model_MemoryDepth = 10
+_Model_MemoryDepth = 30
 _Model_OverloadFactor = 0.8
-_IdentifierStr = 'MACMGPU-sp' + str(_CurrentSprint)
-_RunGroup = 'cp3_sp' + str(_CurrentSprint) + '_MACMGPU26'
+_IdentifierStr = 'MACMGPUQPI-sp' + str(_CurrentSprint)
+_RunGroup = 'cp3_sp' + str(_CurrentSprint) + '_MACMGPU27QPI'
+
+# Following global variables are used by functions below.
+nextNodeID = -13.0
+nextUserID = -21.0
 
 def MultiRun():
 	print("--Begining of Script--")
@@ -51,6 +55,9 @@ def MultiRun():
 	print("--End of Script--")
 
 def Run(file_csv_eventlog, _nodelist_file, file_csv_S3Location, ScenarioNo, Sim_StartTime, Sim_EndTime, Model_MemoryDepth, Model_OverloadFactor, IdentifierStr, CurrentSprint, CommitSha, RunGroup, GroupDesc, RunBy, RunNumber, output_directory_path):
+	global nextNodeID
+	global nextUserID
+
 	if not os.path.exists(output_directory_path):
 		print ("The Output directory path does not exist!")
 		exit()
@@ -63,7 +70,12 @@ def Run(file_csv_eventlog, _nodelist_file, file_csv_S3Location, ScenarioNo, Sim_
 	print('\treading event log file...')
 	originalData = pd.read_csv(file_csv_eventlog,dtype={'userID':str,'conversationID':str,'parentID':str,'nodeID':str},na_values=str)
 	print('\trenaming columns...')
-	originalData.rename(columns={'userID':'nodeUserID','action':'actionType','time':'nodeTime','conversationID':'rootID'},inplace=True)
+	originalData.rename(columns={'userID':'nodeUserID','action':'actionType','time':'nodeTime','conversationID':'rootID','informationIDs':'informationID'},inplace=True)
+	print('\tcalculating next UserID and NodeID values...')
+	nextUserID = np.nansum([originalData['nodeUserID'].apply(pd.to_numeric,errors='coerce').max(), 537])
+	print('\t\tNext User ID : ' + str(nextUserID))
+	nextNodeID = np.nansum([originalData['nodeID'].apply(pd.to_numeric,errors='coerce').max() , 213])
+	print('\t\tNext Node ID : ' + str(nextNodeID))
 
 	# -----------------------------------MAPPING OF EVENTS---------------------
 	platformToEventTypeToEvent = {
@@ -86,6 +98,11 @@ def Run(file_csv_eventlog, _nodelist_file, file_csv_S3Location, ScenarioNo, Sim_
 					"creation": ['message'],
 					"contribution": ["message"],
 					"sharing": ['message']
+				},
+		'youtube':{
+					"creation": ['video'],
+					"contribution": ["comment"],
+					"sharing": ['comment']
 				}
 	}
 
@@ -105,43 +122,152 @@ def Run(file_csv_eventlog, _nodelist_file, file_csv_S3Location, ScenarioNo, Sim_
 
 	#=====================================================================================
 
-	def RemovePlatformFromID(IDString):
-		nameAndID = IDString.split('_',1)
+	def RemovePlatformFromUserID(record):
+		global nextUserID
+		nameAndID = record['nodeUserID'].split('_',1)
 		if nameAndID[0] == 'github' or \
 		nameAndID[0] == 'reddit' or \
 		nameAndID[0] == 'twitter' or \
 		nameAndID[0] == 'telegram':
 			return nameAndID[1]
+		elif record['nodeUserID'] == '-1.0':
+			nextUserID += 1
+			#print("UserID set to " + str(nextUserID))
+			#print(record)
+			return str(nextUserID)
 		else:
-			return IDString
+			return record['nodeUserID']
+	
+	def RemovePlatformFromNodeID(record):
+		global nextNodeID
+		nameAndID = record['nodeID'].split('_',1)
+		if nameAndID[0] == 'github' or \
+		nameAndID[0] == 'reddit' or \
+		nameAndID[0] == 'twitter' or \
+		nameAndID[0] == 'telegram':
+			return nameAndID[1]
+		elif record['nodeID'] == '-1.0':
+			nextNodeID += 1
+			#print("NodeID set to " + str(nextNodeID))
+			#print(record)
+			return str(nextNodeID)
+		else:
+			return record['nodeID']
+
+	def RemovePlatformFromParentID(record):
+		global nextNodeID
+		nameAndID = record['parentID'].split('_',1)
+		if nameAndID[0] == 'github' or \
+		nameAndID[0] == 'reddit' or \
+		nameAndID[0] == 'twitter' or \
+		nameAndID[0] == 'telegram':
+			return nameAndID[1]
+		elif record['parentID'] == '-1.0':
+			nextNodeID += 1
+			if record['actionType'] == 'creation':
+				return record['nodeID']
+			#print("ParentID set to " + str(nextNodeID))
+			#print(record)
+			return str(nextNodeID)
+		else:
+			return record['parentID']
+	
+	def RemovePlatformFromRootID(record):
+		global nextNodeID
+		nameAndID = record['rootID'].split('_',1)
+		if nameAndID[0] == 'github' or \
+		nameAndID[0] == 'reddit' or \
+		nameAndID[0] == 'twitter' or \
+		nameAndID[0] == 'telegram':
+			return nameAndID[1]
+		elif record['rootID'] == '-1.0':
+			nextNodeID += 1
+			if record['actionType'] == 'creation':
+				return record['nodeID']
+			#print("RootID set to " + str(nextNodeID))
+			#print(record)
+			return str(nextNodeID)
+		else:
+			return record['rootID']
 
 	print('\tmapping platform...')
 	originalData['platform'] = originalData.apply(lambda x: x['nodeUserID'].split('_')[0],axis = 1)
+
+	print('\tresolving userid, nodeid, parentid, and rootid...')
+	print("\t\tCurrent NextUserID value: " + str(nextUserID))
+	print("\tresolving userid values...")
+	originalData['nodeUserID'] = originalData.apply(lambda x: RemovePlatformFromUserID(x), axis = 1)
+	print("\t\tCurrent NextUserID value: " + str(nextUserID))
+	print("\t\tCurrent NextNodeID value: " + str(nextNodeID))
+	print('\tresolving nodeid values...')
+	originalData['nodeID'] = originalData.apply(lambda x: RemovePlatformFromNodeID(x), axis = 1)
+	print("\t\tCurrent NextNodeID value: " + str(nextNodeID))
+	print('\tresolving parentid values...')
+	originalData['parentID'] = originalData.apply(lambda x: RemovePlatformFromParentID(x), axis = 1)
+	print("\t\tCurrent NextNodeID value: " + str(nextNodeID))
+	print('\tresolving rootid values...')
+	originalData['rootID'] = originalData.apply(lambda x: RemovePlatformFromRootID(x), axis = 1)
+	print("\t\tCurrent NextNodeID value: " + str(nextNodeID))
+
 	print('\tmappinging actionType...')
-	originalData['actionType'] = originalData.apply(lambda x: MapEventProbablyRandom(x),axis=1)
-	#originalData['informationID'] = ""
-	print('\tremoving platform ids from userid, nodeid, parentid, and rootid...')
-	originalData['nodeUserID'] = originalData.nodeUserID.apply(lambda x: RemovePlatformFromID(x))
-	originalData['nodeID'] = originalData.nodeID.apply(lambda x: RemovePlatformFromID(x))
-	originalData['parentID'] = originalData.parentID.apply(lambda x: RemovePlatformFromID(x))
-	originalData['rootID'] = originalData.rootID.apply(lambda x: RemovePlatformFromID(x))
-	print('\tsetting url parameters...')
+	originalData['actionType'] = originalData.apply(lambda x: MapEventUniformlyRandom(x),axis=1)
+
+	print('\tcreating node to platform map...')
+	node_to_plat = originalData.set_index('rootID').to_dict()['platform']
+	node_to_plat.update(originalData.set_index('parentID').to_dict()['platform'])
+	node_to_plat.update(originalData.set_index('nodeID').to_dict()['platform'])
+
+	PlatformLinkMap = {
+		'twitter':'twitter.com',
+		'github':'github.com',
+		'reddit':'reddit.com',
+		'telegram':'telegram.com',
+		'youtube' : 'youtube.com'
+	}
+
+	def SetHasURL(record):
+		if record['platform'] != node_to_plat[ record['parentID'] ] or record['platform'] != node_to_plat[ record['rootID'] ]:
+			return 1
+		else:
+			return 0
+
+	def SetLinksToExternal(record):
+		if record['platform'] != node_to_plat[ record['parentID'] ] or record['platform'] != node_to_plat[ record['rootID'] ]:
+			return 1
+		else:
+			return 0
+
+	def SetDomainLinked(record):
+		if record['platform'] != node_to_plat[ record['parentID'] ] or record['platform'] != node_to_plat[ record['rootID'] ]:
+			thisUrlList = []
+			if record['platform'] != node_to_plat[ record['parentID'] ] :
+				thisUrlList.append(PlatformLinkMap[ node_to_plat[ record['parentID'] ] ])
+			if record['platform'] != node_to_plat[ record['rootID'] ] :
+				thisUrlList.append(PlatformLinkMap[ node_to_plat[ record['rootID'] ] ])
+			return thisUrlList
+		else:
+			return record['domain_linked']
+
+	print('\tsetting default url parameters...')
 	originalData['urlDomain'] = np.empty((len(originalData),0)).tolist()
 	originalData['has_URL'] = 0
 	originalData['links_to_external'] = 0
 	originalData['domain_linked'] = np.empty((len(originalData),0)).tolist()
+	print('\tapplying url parameter calculations...')
+	originalData['has_URL'] = originalData.apply(lambda x: SetHasURL(x), axis = 1)
+	originalData['links_to_external'] = originalData.apply(lambda x: SetLinksToExternal(x), axis = 1)
+	originalData['domain_linked'] = originalData.apply(lambda x: SetDomainLinked(x), axis = 1)
 
 	# -- Process INFORMATION ID --
 	print('\treading infoid list file...')
 	nodeList = pd.read_csv(_nodelist_file)
-	print('\tsetting random info ids...')
-	originalData['informationID'] = np.random.choice(nodeList[nodeList.columns[0]],originalData.shape[0],replace=True)
-	#originalData['informationID'] = originalData['informationID'].apply(lambda x: nodeList.sample(1).iloc[0].tolist()[0])
 	print('\tcalculating node to infoid and node to parent hash maps...')
 	node_to_info = originalData.set_index('nodeID').to_dict()['informationID']
 	node_to_parent = originalData.set_index('nodeID').to_dict()['parentID']
 	def PickRootInfoID(record):
-		if record['rootID'] in node_to_info.keys():
+		if record['informationID'] != -1.0 or record['informationID'] != '[]':
+			return record['informationID']
+		elif record['rootID'] in node_to_info.keys():
 			return node_to_info[ record['rootID'] ]
 		elif record['parentID'] in node_to_info.keys():
 			currentParent = record['parentID']
@@ -159,6 +285,11 @@ def Run(file_csv_eventlog, _nodelist_file, file_csv_S3Location, ScenarioNo, Sim_
 	print('\tmapping infoids according to cascades...')
 	#return originalData.loc[ originalData['nodeID'] == record['rootID'] , 'informationID' ].append(pd.Series([ record['informationID'] ])).iloc[0]
 	originalData['informationID'] = originalData.apply(lambda x: PickRootInfoID(x), axis = 1)
+
+	# Verification
+	print("\tVERIFICATION...")
+	for col in originalData.columns:
+		print( '\t\t' + str(col) + ' \t\t\t: ' + str(originalData.loc[originalData['nodeUserID'] == '-1.0'].shape[0]) + '\tbad values.')
 
 	print('\twriting output files...')
 	# Write Weird 1st line !!!
