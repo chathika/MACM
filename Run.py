f3fee7ff4f017cf66b5c6fbbcf0f2e6a0a613e7c
deamonpog <pog666@gmail.com>
2020-05-22 00:15:05 -0400

    Copied Run.py from cg/function-dev branch

---
+++ Run.py
@@ -0,0 +1,32 @@
+"""MACM, The Multi-Action Cascade Model of Conversation
+Copyright (C) 2019  Chathika Gunaratne
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
+"""
+                    This code was authored by Chathika Gunaratne
+And is the implementation for the IC2S2 presentation at the University of Amsterdam, 2019
+If you use this code or the MACM model in your work, please cite using the following bibtex:
+
+@inproceedings{gunaratne2019,
+  author =       {Gunaratne, Chathika and Senevirathna, Chathurani and Jayalath, Chathura and Baral, Nisha and Rand, William and Garibay, Ivan},
+  title =        {A Multi-Action Cascade Model of Conversation
+
+},
+  booktitle =    {5th International Conference on Computational Social Science},
+  year =         {2019},
+  url =          {http://app.ic2s2.org/app/sessions/9kXqn5btgKKC5yfCvg/details}
+}
+"""
+"""
+This file can be used to execute a single, manual run of the MACM
+"""
+
