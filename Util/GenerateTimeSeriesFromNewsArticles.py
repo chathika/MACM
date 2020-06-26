3382e0d2459f90aa39fd4a8a9af7521358a540cc
deamonpog <pog666@gmail.com>
2020-06-26 13:26:23 +0000

    added the cp4 news articles exo data processing scripts

---
+++ Util/GenerateTimeSeriesFromNewsArticles.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime as dt
+import glob
+import os
+
+def GenerateTimeSeriesFromNewsArticles(folder_name):
+    file_name = glob.glob(os.path.join(folder_name , '*timeseries*.csv'))[0]
+    df = pd.read_csv(file_name,parse_dates=['date']).rename(columns={'date.1':'count'})
+    df = df[df.date != 'xyz']
+    df['time'] = df.date
+    #df['time'] = df.date.apply(lambda x: dt.strptime(x,'%Y-%m-%d '))
+    df['impact'] = df['count']
+    return pd.pivot_table(df, columns=['slabels'], index='time', values='impact',aggfunc=np.mean).resample('H').mean().ffill().fillna(0)
