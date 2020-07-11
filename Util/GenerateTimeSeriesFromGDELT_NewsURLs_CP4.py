67e0673435c87a05218fc104cfbfcdacd83ad84d
deamonpog <pog666@gmail.com>
2020-07-11 07:56:26 +0000

    optimized by using flashtext and global dataframe

--- Util/GenerateTimeSeriesFromGDELT_NewsURLs_CP4.py
+++ Util/GenerateTimeSeriesFromGDELT_NewsURLs_CP4.py
@@ -9,7 +9,12 @@ import glob
 from pprint import pprint
 import multiprocessing
 import math
+from flashtext.keyword import KeywordProcessor
 
+prog = 0
+indexedKeywords = [] # keywords are the urls
+keywords_to_narratives = {}
+MainDF = 0
 
 def createRegex(iStart, indexedKeywordsSlice):
     pattern = ''
@@ -20,24 +25,29 @@ def createRegex(iStart, indexedKeywordsSlice):
     return pattern
 
 
-def processData(startpos, endpos, prog, indexedKeywords, keywords_to_narratives, MainDF):
+def processData(startpos, endpos):
         if endpos - startpos < 1:
             return 0
         partialTS = []
+        print('\tstarted:{} to {}'.format(startpos,endpos))
         for idx,row in MainDF.iloc[list(np.arange(startpos,endpos))].iterrows():
-            for jdx,v in row.iteritems():
-                for imatch in prog.finditer(str(v)):
-                    if not imatch.lastgroup is None:
-                        kid = int(imatch.lastgroup[2:]) #get id number
-                        for nid in keywords_to_narratives[ indexedKeywords[kid] ]:
-                            partialTS.append([row['day'],nid,row['GoldsteinScale']])
+            for v in ( row['sourceurl'], row['sourceurl_h'] ):
+                for kwmatch in prog.extract_keywords(str(v)):
+                    for nid in keywords_to_narratives[kwmatch]:
+                        partialTS.append([row['day'],nid,row['GoldsteinScale']])
+                # for imatch in prog.finditer(str(v)):
+                #     if not imatch.lastgroup is None:
+                #         kid = int(imatch.lastgroup[2:]) #get id number
+                #         for nid in keywords_to_narratives[ indexedKeywords[kid] ]:
+                #             partialTS.append([row['day'],nid,row['GoldsteinScale']])
+        print('\t\tended:{} to {}'.format(startpos,endpos))
         return partialTS
 
 def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
-    prog = 0
-    indexedKeywords = [] # keywords are the urls
-    keywords_to_narratives = {}
-    MainDF = 0
+    global prog
+    global indexedKeywords
+    global keywords_to_narratives
+    global MainDF
 
     dfArticleNar = pd.read_csv(os.path.join(folder_name,'labeledArticlesWithURLs.csv'))
     indexedUrlList = list(dfArticleNar.url.unique())
@@ -47,17 +57,21 @@ def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
         indexedKeywords.append(row['url'])
     
     # Create regex and compile it for text match
-    print('Creating the regex...')
-    NProcs = multiprocessing.cpu_count()
-    SliceSize = int(math.floor(len(indexedKeywords) / NProcs))
-    paramList = [ (i * SliceSize, indexedKeywords[i * SliceSize : (i + 1) * SliceSize]) for i in range(NProcs - 1) ]
-    paramList.append(( (NProcs - 1) * SliceSize, indexedKeywords[(NProcs - 1) * SliceSize :] ))
-    with multiprocessing.Pool(NProcs) as p:
-        results = p.starmap(createRegex, paramList)
-    pattern = ''.join(results)[:-1]
+    # print('Creating the regex...')
+    # NProcs = multiprocessing.cpu_count()
+    # SliceSize = int(math.floor(len(indexedKeywords) / NProcs))
+    # paramList = [ (i * SliceSize, indexedKeywords[i * SliceSize : (i + 1) * SliceSize]) for i in range(NProcs - 1) ]
+    # paramList.append(( (NProcs - 1) * SliceSize, indexedKeywords[(NProcs - 1) * SliceSize :] ))
+    # with multiprocessing.Pool(NProcs) as p:
+    #     results = p.starmap(createRegex, paramList)
+    # pattern = ''.join(results)[:-1]
+    # print('Compiling the regex...')
+    # prog = re.compile(pattern)
 
-    print('Compiling the regex...')
-    prog = re.compile(pattern)
+    #keyword processor
+    prog = KeywordProcessor()
+    for kw in indexedKeywords:
+        prog.add_keyword(kw)
 
     TS = []
 
@@ -70,7 +84,7 @@ def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
                                         'EventCode', 'NumSources',
                                         'Actor1Geo_CountryCode', 'EventRootCode',
                                         'ActionGeo_Long', 'Actor1Geo_ADM2Code',
-                                        'Actor1Code', 'ActionGeo_CountryCode',
+                                        'Actor1Code', 'ActionGeo_CountryCode', 
                                         'Actor1CountryCode', 'Actor1Geo_FeatureID', 'Actor2Geo_Long',
                                         'IsRootEvent', 'Actor2Geo_CountryCode', 'Actor1Geo_Type',
                                         'Actor2Geo_FeatureID', 'globaleventid', 'ActionGeo_Type',
@@ -83,11 +97,15 @@ def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
                                         'Actor2KnownGroupCode', 'Actor2Type2Code', 'Actor1KnownGroupCode',
                                         'Actor1Religion2Code', 'Actor1Religion1Code', 'Actor2Type3Code',
                                         'Actor2Religion2Code', 'Actor2Religion1Code', 'Actor2EthnicCode',
-                                        'Actor1EthnicCode', 'Actor1Type3Code'])
+                                        'Actor1EthnicCode', 'Actor1Type3Code',
+                                        'Actor1Name', 'Actor2Name', 'ActionGeo_FullName', 'Actor1Geo_FullName', 'Actor2Geo_FullName'
+                                        ])
+        print(MainDF)
+        print(MainDF.columns)
         print('\t\tRows to Proces ' + str(MainDF.shape[0]))
-        NProcs = multiprocessing.cpu_count()
+        NProcs = multiprocessing.cpu_count() - 1
         print('\t\tNum of Procs ' + str(NProcs))
-        sizePerProc = 8000
+        sizePerProc = 1000
         print('\t\tRows per Proc ' + str(sizePerProc))
         rowsPerRun = sizePerProc * NProcs
         print('\t\tRows per Run ' + str(rowsPerRun))
@@ -95,9 +113,9 @@ def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
         currentRowEnd = rowsPerRun
         while currentRowEnd < MainDF.shape[0]:
             print('\t\tProcessing from ' + str(currentRowStart) + ' to ' + str(currentRowEnd))
-            paramList = [(currentRowStart + i * sizePerProc, currentRowStart + (i + 1) * sizePerProc, prog, indexedKeywords, keywords_to_narratives, MainDF) for i in range(NProcs - 1) ]
+            paramList = [(currentRowStart + i * sizePerProc, currentRowStart + (i + 1) * sizePerProc) for i in range(NProcs - 1) ]
             if currentRowEnd > paramList[-1][1]:
-                paramList.append((paramList[-1][1], currentRowEnd))
+                paramList.append((paramList[-1][1], currentRowEnd ))
             print('\t\tAllocation:')
             s = ''
             for p in paramList:
@@ -117,13 +135,13 @@ def GenerateTimeSeriesFromGDELT_NewsURLs_CP4(folder_name):
             print('\t\tremaining rows ' + str(thegap) )
             numProcsReq = thegap // sizePerProc
             print('\t\tRequired Procs ' + str(numProcsReq))
-            paramList = [(currentRowStart + i * sizePerProc, currentRowStart + (i+1) * sizePerProc, prog, indexedKeywords, keywords_to_narratives, MainDF) for i in range(numProcsReq - 1)]
-            print(paramList)
+            paramList = [(currentRowStart + i * sizePerProc, currentRowStart + (i+1) * sizePerProc ) for i in range(numProcsReq - 1)]
+            #print(paramList)
             if paramList == []:
                 paramList = [(currentRowStart, MainDF.shape[0])]
             if MainDF.shape[0] > paramList[-1][1]:
                 paramList.append((paramList[-1][1], MainDF.shape[0]))
-            print(paramList)
+            #print(paramList)
             s = ''
             for p in paramList:
                 s += '[' + str(p[0]) + '-' + str(p[1]) + '),'
